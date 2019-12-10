2f6f104efccb4c76dc482e48fef51a2275456fd4
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-12-09 18:01:44 -0700

    adding svg aspect ratio and viewbox fix

--- src/images/svgs/RunIcon.js
+++ src/images/svgs/RunIcon.js
@@ -10,6 +10,7 @@ export default () => {
     xmlnsXlink="http://www.w3.org/1999/xlink"
     xmlns="http://www.w3.org/2000/svg"
     title="Run"
+    preserveAspectRatio="none"
   >
     <g id="Group">
       <g id="Group">
